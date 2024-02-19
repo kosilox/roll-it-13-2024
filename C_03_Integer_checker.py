ea0201c8537633282dc1e01271814b95bb912f2e
kosilox <159740396+kosilox@users.noreply.github.com>
2024-02-19 13:28:10 +1300

    fixed yes no error message, created int checker > 13`

---
+++ C_03_Integer_checker.py
@@ -0,0 +1,18 @@
+# checks that users enter an integer
+# that is more than 13
+while True:
+
+
+    error = "Please enter an integer that is 13 or more"
+
+    try:
+        my_num = int(input("enter an integer:  "))
+
+        #checks that the number is more than / equal to 13
+        if my_num < 13:
+            print(error)
+        else:
+            print("Your number is", my_num)
+
+    except ValueError:
+        print("Please enter an integer that is 13 or more")
